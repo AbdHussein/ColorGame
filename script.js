057962cf430dea478ddae7264066b2774926ff51
AbdHussein <abdulrahmaanhussein97@gmail.com>
2020-05-17 13:46:33 +0300

    new changes

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 var header = $("#colorDisplay");
 
 function generateColor() {
-	var red = Math.floor(Math.random()*255)
-	var green = Math.floor(Math.random()*255)
-	var blue = Math.floor(Math.random()*255)
-	var rgb = "rgb("+red+","+green+","+blue+")"	
+	var red = Math.floor(Math.random()*255);
+	var green = Math.floor(Math.random()*255);
+	var blue = Math.floor(Math.random()*255);
+	var rgb = "rgb("+red+","+green+","+blue+")";	
 	return rgb;
+}
+
+function generateRandomColors(num){	
+	var arr = [];
+	for(var i = 0; i < num; i++){
+		arr.push(generateColor());
+	}
+	return arr;
 }
 
 header.text(generateColor()); 
