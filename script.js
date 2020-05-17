d16a8673cfc37b8cd481a9845bcf514206eb85ce
AbdHussein <abdulrahmaanhussein97@gmail.com>
2020-05-17 14:59:41 +0300

    1.0

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 var header = $("h1");
 var numOfsquares = 6;
-var squares =$(".square")
-var displaycolors = $("#colorDisplay")
-var messgDisplay = $("#message")
-var reset = $("#reset")
+var squares =$(".square");
+var displaycolors = $("#colorDisplay");
+var messgDisplay = $("#message");
+var reset = $("#reset");
+
+var colors = generateRandomColors(numOfsquares);
+var trueColor = colors[Math.floor(Math.random()*colors.length)];
+header.text(trueColor);
+
+reset.click(function(){
+	init();
+});
+
+init();
+function init(){	
+	fillSquares(colors);
+	setupSquares();
+	//reset();
+}
 
 function generateColor() {
 	var red = Math.floor(Math.random()*255);
@@ -13,7 +28,6 @@ function generateColor() {
 	return rgb;
 }
 
-
 function generateRandomColors(num){	
 	var arr = [];
 	for(var i = 0; i < num; i++){
@@ -21,25 +35,27 @@ function generateRandomColors(num){
 	}
 	return arr;
 }
-var colors = generateRandomColors(numOfsquares);
-var trueColor = colors[Math.floor(Math.random()*colors.length)];
-header.text(trueColor);
 
-function createSquares(){
+function fillSquares(colors){
+	for(var i = 0; i < colors.length; i++){
+		squares[i].style.background = colors[i];
+	}	
+}
+
+function setupSquares(){
 	for(var i =0 ; i <squares.length ; i++){
-		squares[i].on("click",function(){
-			var clickedColor = $(this).style.background
+		squares[i].addEventListener("click",function(event){
+			var clickedColor = $(event.target).css('background');
+			console.log(clickedColor);			
 			if(clickedColor === trueColor){
-				messgDisplay.text("Correct!")
-				reset.text("Play again!")
+				messgDisplay.text("Correct!");
+				reset.text("Play again!");				
 			}
 			else{
-				$(this).style.background = "white"
-				messgDisplay.text("Try again!")
-
+				$(event.target).css('background','white');
+				messgDisplay.text("Try again!");
+				messgDisplay.css('color','white');
 			}
-
 		})
 	}
 }
-
