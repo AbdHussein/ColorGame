ddb2f8f3a739f53edb0ddeb72abf16f18db9cfa6
waelq <waealtaqia20152729@gmail.com>
2020-05-18 10:44:17 +0300

    lastVersion

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var header = $("h1");
 var numOfsquares = 6;
 var squares =$(".square");
 var displaycolors = $("#colorDisplay");
 var messgDisplay = $("#message");
 var reset = $("#reset");
-//---------------
+
 reset.click(function(){	
 	location.reload();
 	init();
@@ -20,7 +19,7 @@ function init(){
 	fillSquares(colors);
 	setupSquares(trueColor);	
 }
-
+//create random color
 function generateColor() {
 	var red = Math.floor(Math.random()*255);
 	var green = Math.floor(Math.random()*255);
@@ -28,7 +27,7 @@ function generateColor() {
 	var rgb = "rgb("+red+", "+green+", "+blue+")";	
 	return rgb;
 }
-
+//Add num of color in array
 function generateRandomColors(num){	
 	var arr = [];
 	for(var i = 0; i < num; i++){
@@ -36,20 +35,20 @@ function generateRandomColors(num){
 	}
 	return arr;
 }
-
+//take each div color from array
 function fillSquares(colors){
 	for(var i = 0; i < colors.length; i++){
 		squares[i].style.background = colors[i];
 	}	
 }
-
+//compare if display color equal any div color
 function setupSquares(trueColor){
 	for(var i =0 ; i < squares.length ; i++){
 		squares[i].addEventListener("click",function(){
 			var clickedColor = $(this).css('background-color');								
 			if(clickedColor === trueColor){		
 				messgDisplay.text("Correct!");
-				messgDisplay.css({'color':'white'})
+				messgDisplay.css({'color':'yellow',"font-size":"20px"})
 				reset.text("Play again?");	
 				for(var i =0 ; i < squares.length ; i++){
 					squares[i].style.background = clickedColor;
@@ -58,7 +57,7 @@ function setupSquares(trueColor){
 			else{
 				$(this).css('background','white');
 				messgDisplay.text("Try again!");
-				messgDisplay.css('color','white');
+				messgDisplay.css({"color":"red","font-size":"20px"});
 			}
 		})
 	}
