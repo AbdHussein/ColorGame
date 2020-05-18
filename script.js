a69f79939605c553d6149f8f0b4ed4935c3555e8
AbdHussein <abdulrahmaanhussein97@gmail.com>
2020-05-18 14:39:54 +0300

    last-version 1.0

--- script.js
+++ script.js
@@ -43,10 +43,12 @@ function setupSquares(trueColor){
 	for(var i =0 ; i < squares.length ; i++){
 		squares[i].addEventListener("click",function(){
 			var clickedColor = $(this).css('background-color');								
-			if(clickedColor === trueColor){		
+			if(clickedColor === trueColor){	
+					
 				messgDisplay.text("Correct!");
 				messgDisplay.css({'color':'yellow',"font-size":"20px"})
 				reset.text("Play again?");	
+
 				for(var i =0 ; i < squares.length ; i++){
 					$(squares[i]).css({'background-color': clickedColor});
 				}			
